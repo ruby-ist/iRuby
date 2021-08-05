531c5dbeeab06dacdf5f19e831968ffe82681aa3
ruby-ist <86733866+ruby-ist@users.noreply.github.com>
2021-08-05 18:06:59 +0530

    send function

--- iRuby_bot.rb
+++ iRuby_bot.rb
@@ -2,13 +2,18 @@ require 'telegram/bot'
 token = ENV['TOKEN']
 Telegram::Bot::Client.run(token) do |bot|
 	bot.listen do |message|
+
+		def send msg
+			bot.api.send_message(chat_id: message.chat.id,text: msg)
+		end
+
 		case message.text.downcase
 			when "hi ruby"
 				bot.api.send_message(chat_id: message.chat.id,text: "Greetings #{message.from.first_name}")
 			when "how are you"
 				bot.api.send_message(chat_id: message.chat.id,text: "Well, I can't say 'Alive', can I?")
 			when "tell me about yourself"
-				bot.api.send_message(chat_id: message.chat.id,text: "Doesn't matter")
+				send "Doesn't matter"
 		end
 	end
 end
