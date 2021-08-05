354af0343aa60cac58abcbd862440af259816fc5
ruby-ist <86733866+ruby-ist@users.noreply.github.com>
2021-08-05 18:16:21 +0530

    removed send function

--- iRuby_bot.rb
+++ iRuby_bot.rb
@@ -1,19 +1,16 @@
 require 'telegram/bot'
-token = ENV['TOKEN']
+token = ENV["TOKEN"]
 Telegram::Bot::Client.run(token) do |bot|
 	bot.listen do |message|
-
-		def send msg
-			bot.api.send_message(chat_id: message.chat.id,text: msg)
-		end
-
 		case message.text.downcase
 			when "hi ruby"
 				bot.api.send_message(chat_id: message.chat.id,text: "Greetings #{message.from.first_name}")
 			when "how are you"
 				bot.api.send_message(chat_id: message.chat.id,text: "Well, I can't say 'Alive', can I?")
 			when "tell me about yourself"
-				send "Doesn't matter"
+				bot.api.send_message(chat_id: message.chat.id,text: "Doesn't matter")
+			when "what is the best romantic movie"
+				bot.api.send_message(chat_id: message.chat.id,text: "Wall-E (2008)")
 		end
 	end
 end
